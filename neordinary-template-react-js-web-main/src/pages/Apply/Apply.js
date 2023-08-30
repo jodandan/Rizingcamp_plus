9b2ea1c290a3466150cce0ab037d93e4474739fa
jodan <wdd789q@naver.com>
2023-08-30 15:48:39 +0900

    feat/Apply 지원하기 기능 구현 완료

--- neordinary-template-react-js-web-main/src/pages/Apply/Apply.js
+++ neordinary-template-react-js-web-main/src/pages/Apply/Apply.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 import ApplyContents from './ApplyContents';
 import AppHeader from '../../components/AppHeader';
 import ApplyBox from './ApplyBox';
+import SubmitTemaplete from './SubmitTemaplete';
 
 const Container = styled.div`
   display: flex;
@@ -33,16 +34,78 @@ const Wrapper = styled.div`
 
 function Apply() {
 
+  const [goToApply, setGoToApply] = useState(false);
+  const [hideContent, setHideContent] = useState(false);
+
+  const [modal, setModal] = useState(false);
+  const [shareModal, setShareModal] = useState(false);
+
+  const [input, setInputs] = useState({
+    nameValue: '',
+    emailValue: '',
+    phoneValue: '',
+    recommendValue: '',
+  });
+  const [nameValid, setNameValid] = useState(false);
+  const [submitList, setSubmitList] = useState([]);
+
+
+  const handleMove = () => {
+    setGoToApply(!goToApply);
+    setHideContent(!hideContent);
+  };
+
+  const toggleApplyBox = () => {
+    setGoToApply(!goToApply);
+  };
+
+  const handleValue = (e) => {
+    // 입력값을 해당 필드의 상태에 업데이트
+    setInputs({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };  
+
+  const handleValid = () => {
+    if (
+      !input.nameValue.length &&
+      !input.emailValue.length &&
+      !input.phoneValue.length
+    ) {
+      setNameValid(!nameValid);
+    }
+  }
+
+  useEffect(() => {
+    const list = JSON.parse(localStorage.getItem('submitList'));
+    setSubmitList(list);
+  }, []);
+
     const { companyId } = useParams();
     return (
         <>
         <AppHeader/>
-        <Container>
-            <Wrapper>
-                <ApplyContents companyId={companyId} />
-                <ApplyBox/>
-            </Wrapper>
-        </Container>
+      <Container>
+        <Wrapper>
+          <ApplyContents companyId={companyId} />
+          {goToApply ? (
+            <SubmitTemaplete
+              goToApply={goToApply}
+              handleMove={toggleApplyBox} // SubmitTemaplete 컴포넌트에서 지원하기 버튼을 누르면 ApplyBox 표시로 변경
+              handleValue={handleValue}
+              nameValid={nameValid}
+              input={input}
+              setSubmitList={setSubmitList}
+            />
+          ) : (
+            <ApplyBox
+              goToApply={goToApply}
+              handleMove={toggleApplyBox} // ApplyBox 컴포넌트에서 지원하기 버튼을 누르면 SubmitTemaplete 표시로 변경
+            />
+          )}
+        </Wrapper>
+      </Container>
       </>
     );
   }
